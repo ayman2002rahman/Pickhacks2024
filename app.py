802e9098352bf29cc22ffe03e0757dfe1b856516
dheeraj9489 <dheerajmb19@gmail.com>
2024-04-06 16:41:30 -0500

    small mod

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET'])
-def home():
+def get_articles():
     return jsonify({"Hello":"World"})
+
+if __name__ == "__main__":
+    app.run()
